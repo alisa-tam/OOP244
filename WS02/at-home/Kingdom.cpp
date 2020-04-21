ccbfcbbcf6b59ae3cf1b6a12b7809fee08243930
Alisa Tam <54546153+alisa-tam@users.noreply.github.com>
2020-04-21 13:54:47 -0400

    Add files via upload

---
+++ WS02/at-home/Kingdom.cpp
@@ -0,0 +1,48 @@
+/***********************************************************
+// OOP244 Workshop 2: Dynamic Memory
+// File Kingdom.cpp
+//Name: Alisa Abramovici
+//Student #: 131580177
+//Date: 2018-05-30
+//Section OOP244SAB*/
+
+
+// HEADER FILES
+#include <iostream>
+#include "Kingdom.h"
+using namespace sict;
+using namespace std;
+
+//NAMESPACE CREATION
+namespace sict {
+
+	//DEFINITION FOR DISPLAY
+	void display(struct Kingdom &pKingdom) {
+		cout << pKingdom.m_name << ", population " << pKingdom.m_population << endl;
+	}
+
+	void display(Kingdom kingdom[], int s) {
+
+		cout << "------------------------------" << endl;
+		cout << "Kingdoms of SICT" << endl;
+		cout << "------------------------------" << endl;
+		
+		int tp = 0;
+		int i;
+
+		for (i = 0; i < s; i++) {
+			cout << i + 1 << ". ";
+			display(kingdom[i]);
+
+			tp += kingdom[i].m_population;
+		}
+		cout << "------------------------------" << endl;
+		cout << "Total population of SICT: " << tp << endl;
+		cout << "------------------------------" << endl;
+
+	}
+}
+	
+
+
+    
---
+++ WS02/at-home/Kingdom.h
@@ -0,0 +1,32 @@
+/***********************************************************
+// OOP244 Workshop 2: Dynamic Memory
+// File Kingdom.cpp
+//Name: Alisa Abramovici
+//Student #: 131580177
+//Date: 2018-05-30
+//Section OOP244SAB*/
+
+// HEADER SAFEGAURDS
+#ifndef SICT_KINGDOM_H
+#define SICT_KINGDOM_H
+
+
+
+// NAMESPACE CREATED
+namespace sict {
+
+	//STRUCTURE DEFINITION FOR KINGDOM
+	struct Kingdom {
+		char m_name[32];
+		int m_population;
+	};
+
+	//DECLERATION OF FUNCTION DISPLAY
+	void display(Kingdom&);
+	void display(Kingdom kingdom[], int s);
+
+}
+
+
+#endif
+
